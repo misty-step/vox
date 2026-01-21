8273cffa320395a859747b211925abe99dafabb1
phrazzld <phrazzld@pm.me>
2026-01-20 18:50:19 -0600

    Remove design lab

--- Sources/VoxApp/AppDelegate.swift
+++ Sources/VoxApp/AppDelegate.swift
@@ -67,7 +67,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
             PermissionManager.promptForAccessibilityIfNeeded()
             Task { _ = await PermissionManager.requestMicrophoneAccess() }
-            HUDDesignLab.openIfEnabled()
         } catch {
             logger.error("Startup failed: \(String(describing: error))")
             showStartupError(String(describing: error))
